9128d0260613b81d77dc03b990789fa19bb45fc3
Maxim <cuwuless@gmail.com>
2022-07-04 15:49:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -107,4 +107,4 @@ def text_wrap(text,font,writing,max_width,max_height):
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="127.0.0.1", port=1337)
